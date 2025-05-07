078f4f0c2b30047a7c9c23cd1608da9f224b56aa
Iuliana-Cherevko <yuliana@eusby.com>
2025-05-07 16:36:20 -0700

    Save changes for 2 files

---
+++ Header Files/Participant.h
@@ -0,0 +1,38 @@
+/*
+    Team name
+    
+    Cherevko, Iuliana (Team Leader)
+    Sviridova, Anatasia 
+    Nguyen, Long
+
+    Spring 2025
+    CS A250 - C++ 2
+
+    Workshop Hub    
+*/
+
+#ifndef PARTICIPANT_H
+#define PARTICIPANT_H
+
+#include <string>
+
+class Participant
+{
+public:
+    Participant(int id, const std::string& theFirstName, const std::string& theLastName)
+        : id(id), firstName(theFirstName), lastName(theLastName) {}
+
+    int getID() const { return id; }
+    std::string getFirstName() const { return firstName; }
+    std::string getLastName() const { return lastName; }
+
+    bool operator< (const Participant& participant) const
+        { return (id < participant.id); }
+
+private:
+    int id;
+    std::string firstName;
+    std::string lastName;
+};
+
+#endif
