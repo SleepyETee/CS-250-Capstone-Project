e1ad2e9c818a714f97654b0a862438178ec30c9a
gdkdll <158369138+gdkdll@users.noreply.github.com>
2025-05-07 20:20:07 -0700

    Update Workshop.h

--- Header Files/Workshop.h
+++ Header Files/Workshop.h
@@ -14,19 +14,22 @@
 #ifndef WORKSHOP_H
 #define WORKSHOP_H
 
-#include <iostream>
+#include <string>
 
 class Workshop {
 public:
-    Workshop(int theNumber, std::string theTitle, int theHours,
-                 int theCapacity, double thePrice);
+    Workshop(int theNumber, const std::string& theTitle, int theHours,
+                 int theCapacity, double thePrice)
+                 : number(theNumber), title(theTitle), hours(theHours),
+                   capacity(theCapacity), price(thePrice) {}
 
     int getNumber() const { return number; };
     std::string getTitle() const { return title; };
     int getHours() const { return hours; };
     int getCapacity() const { return capacity; };
     double getPrice() const { return price; };
-    bool operator<(const Workshop& workshop) { return (number > workshop.number); };
+
+    bool operator<(const Workshop& workshop) const { return number > workshop.number; };
 
 private:
     int number;
