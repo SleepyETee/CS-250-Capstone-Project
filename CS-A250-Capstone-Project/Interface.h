cb7562689d27d61f356a63ef9095c4e51f86522c
gdkdll <158369138+gdkdll@users.noreply.github.com>
2025-05-14 14:58:26 -0700

    Update Interface.h

--- CS-A250-Capstone-Project/Interface.h
+++ CS-A250-Capstone-Project/Interface.h
@@ -1,1 +1,34 @@
-#pragma once
+/*
+Tech Wizards
+
+    Cherevko, Iuliana (Team Leader)
+    Sviridova, Anastasia
+    Nguyen, Long
+
+    Spring 2025
+    CS A250 - C++ 2
+
+    Workshop Hub
+*/
+
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#include "Workshop.h"
+#include "WorkshopList.h"
+#include "ParticipantList.h"
+
+void processMenu(WorkshopList& workshopList,
+                 ParticipantList& participantList,
+                 RegistrationManager& registration);
+
+void getIdentification(int& participantID,
+                       std::string& firstName,
+                       std::string& lastName);
+
+bool verifyIdentification(const ParticipantList& participantList,
+                          int participantID,
+                          const std::string& firstName,
+                          const std::string& lastName);
+
+#endif
